ef4607a98a38439bd5166ec33920490219284bec
umernadim <itsumernadeem@gmail.com>
2025-03-08 20:03:22 -0800

    file added

---
+++ task1.php
@@ -0,0 +1,159 @@
+ <?php
+// Assignment & Arithmetic Operators 
+
+// $a = 10;
+// $b = 5;
+
+// $c = $a + $b;
+// echo "Addition: $c <br>";
+
+// $c = $a - $b;
+// echo "subtraction: $c <br>";
+
+// $c = $a * $b;
+// echo "multiplication $c <br>";
+
+// $c = $a % $b;
+// echo "modulus: $c <br>";
+
+// $c = $a / $b;
+// echo "division: $c <br>";
+
+// 2. Logical Operators
+
+// $age = 20;
+// if($age >= 18 && $age <= 30){
+//     echo 'You are allowed';
+// }
+// else{
+//    echo 'you are not allowed' ;
+// }
+
+//  Simple Grade Calculator
+    // $score = 56;   
+
+    // if ($score >= 90) {
+    //     echo "Grade: A";
+    // } elseif ($score >= 80) {
+    //     echo "Grade: B";
+    // } elseif ($score >= 70) {
+    //     echo "Grade: C";
+    // } elseif ($score >= 60) {
+    //     echo "Grade: D";
+    // } elseif ($score >= 0) {
+    //     echo "Grade: F";
+    // } else {
+    //     echo "Invalid score";
+    // }
+
+// Takes a number and checks if it's positive, negative, or zero.
+
+    // $num = 0;
+    // if($num > 0){
+    //     echo 'The number is positive';
+    // }
+    // else if($num < 0){
+    //     echo 'The number is negative';
+    // }
+    // else{
+    //     echo 'The number is zero';
+    // }
+
+// Even or Odd Checker
+
+    // $num = 10;
+    // if($num % 2 == 0){
+    //     echo 'The number is even';
+    // }
+    // else{
+    //     echo 'The number is odd';
+    // }
+
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>php basic tasks</h1>
+
+    <?php
+// Assignment & Arithmetic Operators 
+
+// $a = 10;
+// $b = 5;
+
+// $c = $a + $b;
+// echo "Addition: $c <br>";
+
+// $c = $a - $b;
+// echo "subtraction: $c <br>";
+
+// $c = $a * $b;
+// echo "multiplication $c <br>";
+
+// $c = $a % $b;
+// echo "modulus: $c <br>";
+
+// $c = $a / $b;
+// echo "division: $c <br>";
+
+// 2. Logical Operators
+
+// $age = 20;
+// if($age >= 18 && $age <= 30){
+//     echo 'You are allowed';
+// }
+// else{
+//    echo 'you are not allowed' ;
+// }
+
+//  Simple Grade Calculator
+    // $score = 56;   
+
+    // if ($score >= 90) {
+    //     echo "Grade: A";
+    // } elseif ($score >= 80) {
+    //     echo "Grade: B";
+    // } elseif ($score >= 70) {
+    //     echo "Grade: C";
+    // } elseif ($score >= 60) {
+    //     echo "Grade: D";
+    // } elseif ($score >= 0) {
+    //     echo "Grade: F";
+    // } else {
+    //     echo "Invalid score";
+    // }
+
+// Takes a number and checks if it's positive, negative, or zero.
+
+    // $num = 0;
+    // if($num > 0){
+    //     echo 'The number is positive';
+    // }
+    // else if($num < 0){
+    //     echo 'The number is negative';
+    // }
+    // else{
+    //     echo 'The number is zero';
+    // }
+
+// Even or Odd Checker
+
+    // $num = 10;
+    // if($num % 2 == 0){
+    //     echo 'The number is even';
+    // }
+    // else{
+    //     echo 'The number is odd';
+    // }
+
+?>
+
+
+</body>
+</html>
